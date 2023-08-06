9bf470f590e399e9dce8e75f3d73c2c085feae06
YingXing <yingxing.cheng@mathematik.uni-stuttgart.de>
2023-08-06 19:57:36 +0200

    fix CI error

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,23 @@
-using Conda
+# using Conda
+# 
+# # Check if scipy is already installed
+# if !("scipy" in Conda._installed_packages())
+#     Conda.add("scipy")
+# end
 
-# Check if scipy is already installed
-if !("scipy" in Conda._installed_packages())
-    Conda.add("scipy")
-end
+#./deps/build.jl
+using Pkg;
+using Logging;
+@info "Initiating build"
+ENV["PYTHON"] = ""
+Pkg.add("Conda")
+Pkg.add("PyCall")
+Pkg.build("PyCall")
+Pkg.build("Conda")
+using PyCall
+using Conda
+## Add the two packages we need
+# Conda.add("gcc=12.1.0"; channel="conda-forge")
+# Conda.add("scikit-image")
+# Pin this version, to avoid clashes with libgcc.34
+Conda.add("scipy")
