792247607360bc3651ab02a2e1d5b70407cd7b61
YingXing <yingxing.cheng@mathematik.uni-stuttgart.de>
2024-12-17 13:16:54 +0100

    backup

--- deps/build.jl
+++ deps/build.jl
@@ -1,23 +1,6 @@
-# using Conda
-# 
-# # Check if scipy is already installed
-# if !("scipy" in Conda._installed_packages())
-#     Conda.add("scipy")
-# end
-
-#./deps/build.jl
-using Pkg;
-using Logging;
-@info "Initiating build"
-ENV["PYTHON"] = ""
-Pkg.add("Conda")
-Pkg.add("PyCall")
-Pkg.build("PyCall")
-Pkg.build("Conda")
-using PyCall
 using Conda
-## Add the two packages we need
-# Conda.add("gcc=12.1.0"; channel="conda-forge")
-# Conda.add("scikit-image")
-# Pin this version, to avoid clashes with libgcc.34
-Conda.add("scipy")
+
+# Check if scipy is already installed
+if !("scipy" in Conda._installed_packages())
+    Conda.add("scipy")
+end
